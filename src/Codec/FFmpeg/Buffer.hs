83a077487838c49f889b0539f626da7c7e9ab079
Vladimir Pankov <wowofbob@gmail.com>
2016-12-31 17:36:10 +0300

    Added wrappers.

--- src/Codec/FFmpeg/Buffer.hs
+++ src/Codec/FFmpeg/Buffer.hs
@@ -4,6 +4,9 @@ module Codec.FFmpeg.Buffer
 ( av_image_get_buffer_size
 , av_image_copy_to_buffer
 , copyImage
+, lineSizeAlign
+, frameBufferSize
+, frameCopyToBuffer
 ) where
 
 import Codec.FFmpeg.Common
@@ -49,24 +52,78 @@ foreign import ccall "av_image_copy_to_buffer"
     -> CInt
     -- Source image height.
     -> CInt
-    -- Line size alignment of destination.
+    -- Line size alignment of source image.
     -> CInt
     -- Number of bytes written to destination.
     -> IO CInt
 
 
--- Returns line size align.
+-- Returns line size alignment. It a width of frame as parameter.
 lineSizeAlign :: CInt -> CInt
-lineSizeAlign lineSize
+lineSizeAlign width
   -- Greater cases are 128 and 256 which are multiple of 32.
-  | lineSize `mod` 32 == 0 = 32
-  | lineSize `mod` 16 == 0 = 16
-  | lineSize `mod` 8  == 0 = 8
-  | lineSize `mod` 4  == 0 = 4
-  | lineSize `mod` 2  == 0 = 2
-  | otherwise              = 1
+  | width `mod` 32 == 0 = 32
+  | width `mod` 16 == 0 = 16
+  | width `mod` 8  == 0 = 8
+  | otherwise           = 1
   
   
+-- Wrapper for av_image_get_buffer_size.
+frameBufferSize :: AVFrame -> IO CInt
+frameBufferSize frame =
+  do
+    
+    -- Frame pixel format.
+    pixelFormat <- getPixelFormat frame
+
+    -- Get width and height of frame.
+    width  <- getWidth  frame
+    height <- getHeight frame
+    
+    -- Alignment of line size.
+    let alignment = lineSizeAlign width
+    
+    -- Supply everything above to av_image_get_buffer_size. 
+    av_image_get_buffer_size
+      pixelFormat
+      width
+      height
+      alignment
+      
+      
+-- Wrapper for av_image_copy_to_buffer. It is assumed that size
+-- of destination buffer is equal to (frameBufferSize givenFrame).
+frameCopyToBuffer :: AVFrame -> Ptr CUChar -> IO CInt
+frameCopyToBuffer frame buffer =
+  do
+    
+    -- Destination buffer size.
+    bufferSize <- frameBufferSize frame
+    -- Source image data.
+    let imageData = hasData frame
+    -- Source image line size.
+    let lineSize = hasLineSize frame
+    -- Source image pixel format.
+    pixelFormat <- getPixelFormat frame
+    -- Source image width.
+    width <- getWidth  frame
+    -- Source image height.
+    height <- getHeight frame
+    -- Line size alignment.
+    let alignment = lineSizeAlign width
+    
+    -- Supply everything above to av_image_copy_to_buffer.
+    av_image_copy_to_buffer
+      buffer
+      bufferSize
+      (castPtr imageData)
+      lineSize
+      pixelFormat
+      width
+      height
+      alignment
+    
+
 {- Returns a byte-string containing an image data.
    
    Returned ByteString doesn't refer back to it's
@@ -82,45 +139,17 @@ copyImage :: AVFrame -> IO ByteString
 copyImage frame =
   do
     
-    -- Get width and height of frame.
-    width  <- getWidth  frame
-    height <- getHeight frame
-    
-    -- Frame pixel format.
-    pixelFormat <- getPixelFormat frame
-    
-    -- Get size of a line in bytes including padding.
-    lineSize <- getLineSize frame
-    
-    -- Get line size alignment.
-    let align = lineSizeAlign lineSize
-    
     -- Get required size of buffer to hold image data.
-    imageBufSize <- av_image_get_buffer_size
-                      pixelFormat
-                      width
-                      height
-                      align
+    imageBufSize <- frameBufferSize frame
                          
     -- Allocate buffer to hold image data.
     imageBuf <- av_malloc $ fromIntegral imageBufSize
     
-    -- Copy image to buffer.
-    _ <- av_image_copy_to_buffer
-          (castPtr imageBuf)
-          imageBufSize
-          -- AVFrame's data pointers.
-          (castPtr $ hasData frame)
-          -- AVFrame's size pointers. 
-          (hasLineSize frame)
-          pixelFormat
-          width
-          height
-          -- Same align for destination.
-          align
-          
     -- Image data buffer cleanup.
     let imageBufCleanup = av_free imageBuf
+    
+    -- Copy image to buffer.
+    _ <- frameCopyToBuffer frame (castPtr imageBuf)
     
     -- Fill up byte-string image by data from buffer.
     image <- unsafePackCStringFinalizer
