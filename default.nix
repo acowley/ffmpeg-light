bec57c6f06d0622a99645a8aaf0480ce0880734b
Anthony Cowley <acowley@gmail.com>
2021-07-15 14:30:34 -0400

    Add nix packaging
    This includes a flake.nix that prepares a development environment
    including haskell-language-server.

---
+++ default.nix
@@ -0,0 +1,41 @@
+{ mkDerivation, nix-filter, base, bytestring, either, exceptions, ffmpeg
+, JuicyPixels, lib, monad-loops, mtl, Rasterific, sdl2
+, stm, text, time, transformers, vector
+}:
+mkDerivation {
+  pname = "ffmpeg-light";
+  version = "0.13.0";
+  src = nix-filter {
+    root = ./.;
+    include = [
+      "./CHANGELOG.md"
+      "demo"
+      "./ffmpeg-light.cabal"
+      "./LICENSE"
+      "./README.md"
+      "./Setup.hs"
+      "./stack.yaml"
+      "src"
+      (nix-filter.inDirectory "src")
+      (nix-filter.inDirectory "demo")
+    ];
+  };
+  configureFlags = [
+    "-fbuildaudioextractdemo" "-fbuildaudiosindemo" "-fbuilddemo"
+    "-fbuildrasterdemo" "-fbuildtranscodedemo" "-fbuildvplaydemo"
+  ];
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base bytestring either exceptions JuicyPixels mtl stm transformers
+    vector
+  ];
+  libraryPkgconfigDepends = [ ffmpeg ];
+  executableHaskellDepends = [
+    base bytestring JuicyPixels monad-loops mtl Rasterific sdl2 text
+    time transformers vector
+  ];
+  homepage = "http://github.com/acowley/ffmpeg-light";
+  description = "Minimal bindings to the FFmpeg library";
+  license = lib.licenses.bsd3;
+}
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Haskell wrapper for the ffmpeg library";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { 
+            inherit system;
+          };
+          compiler = "ghc8104";
+          hspkgs = pkgs.haskell.packages.${compiler};
+          ffmpeg-light = hspkgs.callPackage (import ./default.nix) { nix-filter = nix-filter.lib; };
+          ghc = hspkgs.ghc.withHoogle (ps: ffmpeg-light.passthru.getBuildInputs.haskellBuildInputs);
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.SDL2 pkgs.ffmpeg pkgs.pkgconfig
+            ghc hspkgs.cabal-install hspkgs.haskell-language-server
+          ];
+        };
+        packages.ffmpeg-light = ffmpeg-light;
+        defaultPackage = self.packages.${system}.ffmpeg-light;
+      }
+    );
+}
